9337e66260d493c7b72217520f424fedf6f9fad1
jinu <jinwoo794533@gmail.com>
2022-03-21 12:28:20 +0900

    Auto stash before merge of "main" and "origin/main"

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Dynamicsk
 
+<<<<<<< Updated upstream
 ## 구성
 ### [1. 기획 배경](#기획-배경)
 ### [2. Flowchart](#플로우차트)
@@ -14,4 +15,6 @@
 ![flowchart](https://github.com/qj0r9j0vc2/Dynamicsk/blob/main/Dynamicsk-FlowChart.png)
 
 ## **구현**
+=======
+>>>>>>> Stashed changes
 
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+)
 
+func main() {
+	local := getOutboundIP()
+	conn, err := net.Dial("tcp", local.String()+":8379")
+	if err != nil {
+		StartServer()
+		conn, err = net.Dial("tcp", ":8379")
+	}
+
+}
+
+func StartServer(ip string) {
+	server, err := net.Listen("tcp", ip)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+func proxy(from)
+
+func getOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
